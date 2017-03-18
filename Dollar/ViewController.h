5269ad327c15ebac24b516acb3b66fcc303e762a
user <user@mini-1.local>
2017-03-18 11:15:56 +0400

    First lab with this shit

--- Dollar/ViewController.h
+++ Dollar/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)getCourse:(id)sender;
 
 @end
 
--- Dollar/ViewController.m
+++ Dollar/ViewController.m
@@ -23,6 +23,28 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+}
+
+/* получаем курс валют */
+- (IBAction)getCourse:(id)sender {
+    
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: self delegateQueue: [NSOperationQueue mainQueue]];
+    
+    NSURL * url = [NSURL URLWithString:@"http://www.cbr.ru/scripts/XML_daily.asp?"];
+    
+    NSURLSessionDataTask * dataTask = [defaultSession dataTaskWithURL:url
+                 completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                 if(error == nil)
+                 {
+                   NSString * text = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+                   NSLog(@"Data = %@", text);
+                 }
+    }];
+    
+    [dataTask resume];
 }
 
 
