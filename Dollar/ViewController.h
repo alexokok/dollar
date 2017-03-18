cb5bf57468abbc325173caa0e75496e109fd35fe
user <user@mini-1.dlink>
2017-03-18 08:41:38 +0400

    Initial Commit

---
+++ Dollar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dollar
+//
+//  Created by user on 18.03.17.
+//  Copyright © 2017 HelloWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dollar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Dollar
+//
+//  Created by user on 18.03.17.
+//  Copyright © 2017 HelloWorld. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
